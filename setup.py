b27722bfbf26b34b55c8f87c840479ad0e0f1057
Rick Henry <fredericmhenry@gmail.com>
2017-07-31 02:00:08 -0400

    change requirements
    Signed-off-by: Rick Henry <fredericmhenry@gmail.com>

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ setup(
     author_email='fredericmhenry@gmail.com',
 
     license='GPLv3',
-    python_requires='>=3',
+    python_requires='>=2.7',
 
     packages=find_packages(),
     entry_points={
