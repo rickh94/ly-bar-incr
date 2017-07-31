700d8e93910cfb7bb240af6b8ec172c83cec0933
Rick Henry <fredericmhenry@gmail.com>
2017-07-31 15:27:55 -0400

    bump version number
    Signed-off-by: Rick Henry <fredericmhenry@gmail.com>

--- setup.py
+++ setup.py
@@ -9,16 +9,16 @@ import os
 here = path.abspath(path.dirname(__file__))
 
 with open(path.join(here, 'README.md'), encoding='utf-8') as f:
-        long_description = f.read()
+    long_description = f.read()
 
 setup(
     name='ly-bar-incr',
-    version='0.1',
+    version='0.2',
 
     description='Increment bar numbers in comments and bar number checks of a lilypond file.',
     long_description=long_description,
     url='https://github.com/rickh94/ly-bar-incr',
-    
+
     author='Rick Henry',
     author_email='fredericmhenry@gmail.com',
 
